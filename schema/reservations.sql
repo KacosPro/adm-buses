150ac2dfbe17f40a6c5a535b5584766268b04981
Carlos <sbncacos@hotmail.com>
2016-03-06 20:22:10 -0600

    Update schema

--- schema/reservations.sql
+++ schema/reservations.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS `reservaciones` (
   `destino` varchar(100) NOT NULL,
   `fecha_hora` datetime NOT NULL,
   `ruta_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `ruta_id` (`ruta_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf32;
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
